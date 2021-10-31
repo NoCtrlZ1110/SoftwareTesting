cb24b914402287ace54aa85823a5c38b4e156c2d
NoCtrlZ1110 <huynd.nguyen@gmail.com>
2021-11-01 03:39:35 +0700

    add login & create post test

--- realworld-server/models/User.js
+++ realworld-server/models/User.js
@@ -85,7 +85,7 @@ UserSchema.methods.toProfileJSONFor = function (user) {
 
 UserSchema.methods.favorite = function (id) {
   if (this.favorites.indexOf(id) === -1) {
-    this.favorites.push(id);
+    this.favorites.unshift(id);
   }
 
   return this.save();
